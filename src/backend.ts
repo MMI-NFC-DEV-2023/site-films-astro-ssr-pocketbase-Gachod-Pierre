949e279f25571e4a06ee95e338caf7962852186b
Gachod-Pierre <pierre_jimmy_laurent.gachod-martin@edu.univ-fcomte.fr>
2025-03-10 14:33:00 +0100

    add: variable d'env public

--- src/backend.ts
+++ src/backend.ts
@@ -2,5 +2,5 @@ import PocketBase from "pocketbase";
 import type { TypedPocketBase } from "./pocketbase-typegen";
 
 export const pb = new PocketBase(
-  import.meta.env.VITE_URL_POCKETBASE
+  import.meta.env.PUBLIC_POKEAPI
 ) as TypedPocketBase;
